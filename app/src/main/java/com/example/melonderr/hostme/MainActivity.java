0d88cd5978ddaf1861a09c4d8d9e73fea205b9ad
Srisri Gokanapudy <sgoka001@ucr.edu>
2017-10-28 17:35:51 -0700

    button stuff

--- app/src/main/java/com/example/melonderr/hostme/MainActivity.java
+++ app/src/main/java/com/example/melonderr/hostme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.melonderr.hostme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.submit_button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if( ed1.getText().toString().equals("admin") &&
+                        ed2.getText().toString().equals("admin")) {
+                }else{
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
     }
 }
