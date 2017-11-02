14eb30876fb9b9274344e3ad3671f03c4f8e7ab7
Japneet Kaur <japneetkaur@ucrwpa1-3-10-25-9-158.wnet.ucr.edu>
2017-11-01 20:52:35 -0700

    japneet first commit

---
+++ app/src/main/java/com/example/melonderr/hostme/RestuarantMainPage.java
@@ -0,0 +1,13 @@
+package com.example.melonderr.hostme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestuarantMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restuarant_main_page);
+    }
+}
