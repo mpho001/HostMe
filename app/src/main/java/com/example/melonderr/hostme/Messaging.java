da14808d70a65e0dca009a3ac0cd11d4036c618d
Japneet Kaur <japneetkaur@ucrwpa1-3-10-25-9-158.wnet.ucr.edu>
2017-11-01 21:07:25 -0700

    messaging

---
+++ app/src/main/java/com/example/melonderr/hostme/Messaging.java
@@ -0,0 +1,13 @@
+package com.example.melonderr.hostme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Messaging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
