27a8b3af5288d5c5dde48f86fd86a367ceed493f
Srisri Gokanapudy <sgoka001@ucr.edu>
2017-11-05 16:15:09 -0800

    created Restaurant_Search Activity

---
+++ app/src/main/java/com/example/melonderr/hostme/Restaurant_Search.java
@@ -0,0 +1,22 @@
+package com.example.melonderr.hostme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Restaurant_Search extends AppCompatActivity {
+    //protected GeoDataClient mGeoDataClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant__search);
+        /*
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // TODO: Start using the Places API.
+        */
+    }
+}
