62929dab5a0a6d620812302090ce5dd661bfff95
jlee368 <jlee368@ucr.edu>
2017-11-08 19:59:25 -0800

    Created User main page and Custom Grid class

--- app/src/main/java/com/example/melonderr/hostme/User_Main_Page.java
+++ app/src/main/java/com/example/melonderr/hostme/User_Main_Page.java
@@ -1,27 +1,73 @@
 package com.example.melonderr.hostme;
 
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.os.Bundle;
 
-public class User_Main_Page extends AppCompatActivity {
+
+
+public class User_Main_Page extends Activity {
+
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_user__main__page);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+            GridView grid;
+            String[] web = {
+                    "My account"
+//            "Github",
+//            "Instagram",
+//            "Facebook",
+//            "Flickr",
+//            "Pinterest"
+
+    } ;
+            int[] imageId = {
+                    R.drawable.btn_profile
+        //            R.drawable.image2,
+        //            R.drawable.image3,
+        //            R.drawable.image4,
+        //            R.drawable.image5,
+        //            R.drawable.image6
+
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__main__page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        CustomGrid adapter = new CustomGrid(User_Main_Page.this, web, imageId);
+        grid=(GridView)findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(User_Main_Page.this, "You Clicked at " +web[+ position], Toast.LENGTH_SHORT).show();
+
             }
         });
     }
