40842c206836f18d88378c311f2dd5e3178b9b0a
Japneet Kaur <japneetkaur@ucrwpa1-3-10-25-9-116.wnet.ucr.edu>
2017-11-15 13:11:41 -0800

    reservation

--- app/src/main/java/com/example/melonderr/hostme/my_account_info.java
+++ app/src/main/java/com/example/melonderr/hostme/my_account_info.java
@@ -6,9 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class my_account_info extends AppCompatActivity {
-
+    private Button changePasswordButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,14 @@ public class my_account_info extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        changePasswordButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(my_account_info.this, changePassword.class));
+
+            }
+        });
     }
+
 
 }
