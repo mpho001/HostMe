45312b2e1cc1a1ed3cb730b5a272875d9ca16ba7
jlee368 <jlee368@ucr.edu>
2017-11-15 15:17:45 -0800

    Updated Registration/error sign included

--- app/src/main/java/com/example/melonderr/hostme/Registration.java
+++ app/src/main/java/com/example/melonderr/hostme/Registration.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import android.content.Intent;
@@ -25,7 +26,7 @@ import java.util.List;
 
 public class Registration extends AppCompatActivity {
     DatabaseHelper myDb;
-    EditText editName,editSurname,editPhone ,editTextId, editTextQuestion,editEmail,editAnswer;
+    EditText editName,editSurname,editPhone,editPassword,editComfirmPassword ,editTextId, editTextQuestion,editEmail,editAnswer;
     Button btnAddData;
     Button btnSignupGoogle;
     Button btnviewAll;
@@ -44,6 +45,8 @@ public class Registration extends AppCompatActivity {
         editSurname = (EditText)findViewById(R.id.LastName);
         editPhone = (EditText)findViewById(R.id.PhoneNumber);
         editEmail = (EditText) findViewById(R.id.email_address);
+        editPassword = (EditText) findViewById(R.id.Password);
+        editComfirmPassword = (EditText) findViewById(R.id.ConfirmPassword);
 //        editTextId = (EditText)findViewById(R.id.editText_id);
 //        editTextQuestion = (EditText)findViewById(R.id.Security_Questions);
         editAnswer = (EditText) findViewById(R.id.Security_Answer);
@@ -58,8 +61,15 @@ public class Registration extends AppCompatActivity {
         String[] items = new String[]{"What was the name of your elementary / primary school?", "In what city or town does your nearest sibling live?", "What was your childhood nickname?"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         SecurityQuestion.setAdapter(adapter);
-//        SecurityQuestion.setOnItemSelectedListener(this);
-        AddData();
+//        SecurityQuestion.setOnItemSelectedListener(new Listener_Spinner());
+
+        btnAddData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                AddData();
+            }
+        });
         viewAll();
         UpdateData();
         DeleteData();
@@ -83,6 +93,9 @@ public class Registration extends AppCompatActivity {
 //        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //        SecurityQuestion.setAdapter(dataAdapter);
 //    }
+//    public static class Listener_Spinner implements AdapterView.OnItemClickListener{
+//
+//    };
     public void DeleteData() {
 //        btnDelete.setOnClickListener(
 //                new View.OnClickListener() {
@@ -111,27 +124,100 @@ public class Registration extends AppCompatActivity {
 //                        else
 //                            Toast.makeText(Registration.this,"Data not Updated",Toast.LENGTH_LONG).show();
 //                    }
-//                }
 //        );
     }
+//                }
     public  void AddData() {
-        btnAddData.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        boolean isInserted = myDb.insertData(editName.getText().toString(),
-                                editSurname.getText().toString(),editPhone.getText().toString(), "", "" ,"" ,"","", "",
-                                /*editTextQuestion.getText().toString()*/ "" ,"", "" ,"" ,"","");
-                        if(isInserted == true) {
-                            Toast.makeText(Registration.this,"Registered",Toast.LENGTH_LONG).show();
-                            Intent i = new Intent(Registration.this, MainActivity.class);
-                            startActivity(i);
-                        }
-                        else
-                            Toast.makeText(Registration.this,"Recheck Your Input",Toast.LENGTH_LONG).show();
-                    }
-                }
-        );
+        if(!validate())
+        {
+            onSignupFailed();
+            return;
+        }
+
+//        btnAddData.setEnabled(false);
+//        btnAddData.setOnClickListener(
+//                new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+            boolean isInserted = myDb.insertData(editName.getText().toString(),
+                    editSurname.getText().toString(),editPhone.getText().toString(), editEmail.getText().toString(), editPassword.getText().toString()
+                    ,editPassword.getText().toString() ,"","", "",
+                    /*editTextQuestion.getText().toString()*/ "" ,editAnswer.getText().toString());
+            if(isInserted == true) {
+                Toast.makeText(Registration.this,"Registered",Toast.LENGTH_LONG).show();
+                Intent i = new Intent(Registration.this, MainActivity.class);
+                startActivity(i);
+            }
+            else
+                Toast.makeText(Registration.this,"Recheck Your Input",Toast.LENGTH_LONG).show();
+        }
+//    }
+//        );
+//    }
+    public void onSignupFailed()
+    {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+       btnAddData.setEnabled(true);
+    }
+    public boolean validate(){
+        boolean valid = true;
+        String _First = editName.getText().toString();
+        String _Last = editSurname.getText().toString();
+        String _Phone = editPhone.getText().toString();
+        String _Email = editEmail.getText().toString();
+        String _Password = editPassword.getText().toString();
+        String _ConfirmPassword = editComfirmPassword.getText().toString();
+//        String _Question =
+        String _Answer = editAnswer.getText().toString();
+        if(_First.isEmpty() || _First.length() < 2){
+            editName.setError("At least 2 characters");
+            valid = false;
+        }
+        else editName.setError(null);
+
+        if(_Last.isEmpty() || _Last.length() < 2){
+            editSurname.setError("At least 2 characters");
+            valid = false;
+        }
+        else editSurname.setError(null);
+
+        if(_Phone.isEmpty() || _Phone.length() < 9){
+            editPhone.setError("Enter a valid phone number");
+            valid = false;
+        }
+        else editPhone.setError(null);
+
+        if(_Email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(_Email).matches() ){
+            editEmail.setError("Enter a valid email address");
+            valid = false;
+        }
+        else editName.setError(null);
+
+        if(_Password.isEmpty() || _Password.length() < 4){
+            editPassword.setError("At least 4 characters");
+            valid = false;
+        }
+        else editPassword.setError(null);
+
+
+        if(_ConfirmPassword.isEmpty() || !_Password.equals(_ConfirmPassword)){
+            editComfirmPassword.setError("Passwords to not match");
+            valid = false;
+        }
+        else editComfirmPassword.setError(null);
+
+        if(_Answer.isEmpty()||_Answer.length() < 2){
+            editAnswer.setError("At least 2 characters");
+            valid = false;
+        }
+        else editAnswer.setError(null);
+
+
+
+
+
+
+        return valid;
     }
 
     public void viewAll() {
