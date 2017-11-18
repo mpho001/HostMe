74a024294160068c13cf081b9fc7a7ebee17a95a
Srisri Gokanapudy <sgoka001@ucr.edu>
2017-11-18 14:34:41 -0800

    reserve and review buttons go to new activities

--- app/src/main/java/com/example/melonderr/hostme/RestuarantMainPage.java
+++ app/src/main/java/com/example/melonderr/hostme/RestuarantMainPage.java
@@ -1,7 +1,10 @@
 package com.example.melonderr.hostme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestuarantMainPage extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class RestuarantMainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restuarant_main_page);
+
+        Button accountBtn = findViewById(R.id.reviews);
+        accountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RestuarantMainPage.this, Review.class);
+                startActivity(i);
+            }
+        });
+
+        Button reservationButton = findViewById(R.id.reserve);
+        reservationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RestuarantMainPage.this, Reserve.class);
+                startActivity(i);
+            }
+        });
     }
 }
